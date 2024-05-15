8b46727591dd3e4b8856e264a2e1ec1a3f5e9e5c
jmlinares2019 <jlinaresmar@gmail.com>
2024-05-15 19:26:36 +0200

    Adding Title service for browser tab titles

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+
+  constructor(private titleService: Title){
+    this.titleService.setTitle("John Doe - Contact");
+  }
 
 }
