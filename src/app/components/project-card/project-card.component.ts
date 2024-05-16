b8b4698409c5e97fde88ef6c68a5d9b9c6ca7680
jmlinares2019 <jlinaresmar@gmail.com>
2024-05-16 18:07:01 +0200

    Creating Project interface for inputting data in project-card component

--- src/app/components/project-card/project-card.component.ts
+++ src/app/components/project-card/project-card.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Project } from '../../_models/Project';
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.scss'
 })
+
 export class ProjectCardComponent {
+  @Input() project = {} as Project; 
 
 }
