cf805b278aaeba76cc6be5def6de660107c6c9b7
jmlinares2019 <jlinaresmar@gmail.com>
2024-05-17 03:02:17 +0200

    Automatically (OnInit) getting list of projects from ProjectsService at portfolio page

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { ProjectsService } from './../../_services/projects.service';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Project } from '../../_models/Project';
+// import { Tag } from '../../_models/Tag';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit{
 
-  constructor(private titleService: Title){
+  constructor(private titleService: Title, private ProjectsService: ProjectsService){
     this.titleService.setTitle("John Doe - Portfolio");
+  }
+
+  projects = {} as Project[];
+
+  ngOnInit(): void { 
+    // getProjects when the component is initialized 
+    // (without clicking any button or performing any action)
+    this.projects = this.ProjectsService.getProjects();
   }
 
 }
