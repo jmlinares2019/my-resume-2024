dacf990a487d0179dc9206a2972d52c1deda7095
jmlinares2019 <jlinaresmar@gmail.com>
2024-05-21 20:55:42 +0200

    Adding download resume functionality to resume page

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
@@ -8,7 +8,16 @@ import { Title } from '@angular/platform-browser';
 })
 export class ResumeComponent {
   
-  constructor(private titleService: Title){
+  constructor(private titleService: Title, private renderer: Renderer2){
     this.titleService.setTitle("John Doe - Resume");
+  }
+
+  downloadFile(){
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../../assets/resume_2023.pdf');
+    link.setAttribute('download', 'resume_2023.pdf');
+    link.click();
+    link.remove();
   }
 }
