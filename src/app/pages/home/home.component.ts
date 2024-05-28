42477323fb6425f0607ff06e3ddeec78bcc04df6
jmlinares2019 <jlinaresmar@gmail.com>
2024-05-28 04:13:32 +0200

    - Styling Home (& placing there Resume component) - Styling Resume component & subcomponents

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,7 @@
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ProjectsService } from './../../_services/projects.service';
 import { Project } from '../../_models/Project';
-import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -10,14 +10,24 @@ import { Title } from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private titleService: Title, private ProjectsService: ProjectsService){
+  constructor(private titleService: Title, private ProjectsService: ProjectsService, private renderer: Renderer2, ){
     this.titleService.setTitle("John Doe - Home");
   }
 
   featuredProject = {} as Project;
 
   ngOnInit(): void {
-    this.featuredProject = this.ProjectsService.getProjectById(1);
+    // this.featuredProject = this.ProjectsService.getProjectById(1);
+  }
+
+  // Download PDF file
+  downloadFile(){
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../../assets/resume_2023.pdf');
+    link.setAttribute('download', 'resume_2023.pdf');
+    link.click();
+    link.remove();
   }
 
   
